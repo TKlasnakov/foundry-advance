5711eb57bc82fd77c248c1c8aebd9ba3f2065583
tklasnakov <t.klasnakov@gmail.com>
2025-02-09 12:44:28 +0200

    Create deploy script

---
+++ nft/script/DeployMoodNft.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { Script, console } from "forge-std/Script.sol";
+import { MoodNft } from "../src/MoodNft.sol";
+import { Base64 } from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract DeployMoodNft is Script {
+
+	function run() external returns(MoodNft) {
+		string memory happySvg = svgToImageUri(vm.readFile("./img/happy.svg"));
+		string memory sadSvg = svgToImageUri(vm.readFile("./img/sad.svg"));
+
+		vm.startBroadcast();
+		MoodNft moodNft = new MoodNft(happySvg, sadSvg);
+		vm.stopBroadcast();
+
+		return moodNft;
+	} 
+
+	function svgToImageUri(string memory svg) public pure returns (string memory) {
+		 string memory baseUrl = "data:image/svg+xml;base64,";
+		 string memory svgBase64Encoded = Base64.encode(
+			 bytes(string(abi.encodePacked(svg)))
+		 );
+
+		 return string(abi.encodePacked(baseUrl, svgBase64Encoded));
+	}
+
+}
